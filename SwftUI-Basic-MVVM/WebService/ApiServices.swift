1707a0811afe1545127761577ab858dda1113b24
Luthfi Abdul Azis <luthfieabdulazis@gmail.com>
2020-04-30 03:12:52 +0700

    Add services

---
+++ SwftUI-Basic-MVVM/WebService/ApiServices.swift
@@ -0,0 +1,17 @@
+//
+//  ApiServices.swift
+//  SwftUI-Basic-MVVM
+//
+//  Created by Luthfi Abdul Azis on 30/04/20.
+//
+
+import Foundation
+
+class ApiServices {
+    
+    func load(url: URL) {
+        
+        
+        
+    }
+}
