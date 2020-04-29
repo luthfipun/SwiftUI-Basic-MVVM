55a96d9246249e891856a5aac82fe8e8853cfcbf
Luthfi Abdul Azis <luthfieabdulazis@gmail.com>
2020-04-30 03:01:24 +0700

    Initial Commit

---
+++ SwftUI-Basic-MVVM/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwftUI-Basic-MVVM
+//
+//  Created by Luthfi Abdul Azis on 30/04/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
